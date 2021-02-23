8d52fa651a10dd222db2101a157f2857ffbf3f49
Phillmont Muktar <51525686+purfectliterature@users.noreply.github.com>
2021-02-24 00:29:06 +0800

    Project cleanup and added Netlify _redirects

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,8 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="This is a trunk deployment"/>
+    
   </div>
 </template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
 <style>
 #app {
