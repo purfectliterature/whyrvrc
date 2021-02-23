89ee12d61eaae787180094eae6b0bcf3a479a848
Phillmont Muktar <51525686+purfectliterature@users.noreply.github.com>
2021-02-23 23:56:30 +0800

    Added note for trunk branch

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="This is a trunk deployment"/>
   </div>
 </template>
 
