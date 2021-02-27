f5eb218d97168b0fe35d8ddd9d3416ad4cac969e
Phillmont Muktar <51525686+purfectliterature@users.noreply.github.com>
2021-02-28 02:32:49 +0800

    Added intro shield onto HomePage

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,15 +1,17 @@
 <template>
   <div>
     <hero />
+    <intro-shield />
     <div class="bg-blue-700 h-60"></div>
   </div>
 </template>
 
 <script>
 import Hero from '@/components/Hero.vue';
+import IntroShield from '@/components/IntroShield.vue';
 
 export default {
-  components: { Hero },
+  components: { Hero, IntroShield },
   name: "HomePage"
 }
 </script>
