a947e0cd2d996245f77b3b80d29f18dff1639fb4
Phillmont Muktar <51525686+purfectliterature@users.noreply.github.com>
2021-02-28 03:04:28 +0800

    Added angle-down animation

--- src/components/ScrollDownIndicator.vue
+++ src/components/ScrollDownIndicator.vue
@@ -2,7 +2,7 @@
   <div class="gradient flex flex-col items-center text-white w-full pb-4">
     <p class="block uppercase tracking-wider pb-1">Discover your potential</p>
     <unicon class="hidden sm:block" name="mouse-alt" fill="white" width="2rem" height="2rem" />
-    <unicon name="angle-down" fill="white" width="1.5rem" height="1.5rem" />
+    <unicon class="angle-down" name="angle-down" fill="white" width="1.5rem" height="1.5rem" />
   </div>
 </template>
 
@@ -13,6 +13,27 @@ export default {
 </script>
 
 <style scoped>
+.angle-down {
+  animation: sliding 2s linear infinite;
+}
+
+@keyframes sliding {
+  0% { 
+    transform: translateY(0rem);
+    opacity: 0; 
+  }
+
+  50% {
+    transform: translateY(0.5rem);
+    opacity: 1;
+  }
+
+  100% {
+    transform: translateY(1rem);
+    opacity: 0; 
+  }
+}
+
 @media (min-width: 768px) {
   .gradient {
     background: linear-gradient(to bottom, transparent, #0F0F0E);
