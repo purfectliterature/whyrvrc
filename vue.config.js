12c42acc1272094929a6ed573b3ba44ca1b1ae13
Phillmont Muktar <51525686+purfectliterature@users.noreply.github.com>
2021-03-21 22:01:26 +0800

    dev: updated site title with RVRC in front

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@ module.exports = {
     index: {
       entry: "src/main.js",
       template: "public/index.html",
-      title: "Discover your potential at Ridge View Residential College"
+      title: "RVRC - Discover your potential"
     }
   }
 }
