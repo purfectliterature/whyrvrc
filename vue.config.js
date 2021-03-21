29da5667d171115dc0f9959d770b4980ea9e1153
Phillmont Muktar <51525686+purfectliterature@users.noreply.github.com>
2021-03-21 21:52:44 +0800

    dev: changed page title

---
+++ vue.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  pages: {
+    index: {
+      entry: "src/main.js",
+      template: "public/index.html",
+      title: "Discover your potential at Ridge View Residential College"
+    }
+  }
+}
