ea5f925bb8d274cdbd2033458de439763735b711
Phillmont Muktar <51525686+purfectliterature@users.noreply.github.com>
2021-03-21 18:34:24 +0800

    feat(ThisYearSection): added text contents

--- src/components/ThisYearSection.vue
+++ src/components/ThisYearSection.vue
@@ -6,14 +6,16 @@
       md:text-center md:items-center
     ">
       <div class="space-y-1">
-        <h2>Even when life gets hard,</h2>
-        <p class="font-extended font-extrabold text-7xl lg:text-8xl">we'll overcome together.</p>
+        <h2>And even if this year has been tough,</h2>
+        <p class="font-extended font-extrabold text-7xl lg:text-8xl">fun still exists.</p>
       </div>
 
       <p class="text-lg this-year-description">
-        RVRC is the first residential college at NUS to stay open despite others want to close.
-        I know we are cool and all, and you can be sure that we always ready with all the safe 
-        management measures. Just declare your temperature, you won’t be kicked out.
+        RVRC is the first residential college at NUS to restore face-to-face physical classes, all conducted
+        safely with compliance of safe management measures. Physical interactions are important in your experiential 
+        learning and living here, and thus the college enforces and maintains a high standard of safe management measures.
+        With these measures, you can continue to learn and live your precious university life smoothly and safely, and your
+        loved ones can have the peace of mind that you will be catered for doing so.
       </p>
     </div>
 
@@ -24,7 +26,7 @@
     <div class="px-5 pb-8 flex flex-row items-center justify-center md:flex-col md:items-start md:justify-start md:w-4/5 md:pr-0">
       <unicon name="image-v" fill="#BDBDBD" width="1.5rem" height="1.5rem" />
       <p class="memory-description text-memory-light-gray font-medium text-sm leading-snug ml-3 md:ml-0 md:mt-1">
-        Photo of people partying like it’s 2020
+        RVRC residents having a late-night supper party on the RVRC Lawn, all exuberantly mingling while respecting safe management measures.
       </p>
     </div>
   </section>
